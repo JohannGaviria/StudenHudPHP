427ee0f69119a14e7a99466efb8570fa1e29d63e
JohannGaviria <johannmgr@gmail.com>
2023-10-06 13:14:39 -0500

    se crea el buscador en las ofertas de trabajo

--- public/job-offers.php
+++ public/job-offers.php
@@ -10,17 +10,51 @@ if (!isset($_SESSION["user_id"])) {
 
 $type = 'job_offers';
 
-?>
+include "../config/database.php"; 
 
-<?php include_once('../templates/php/info-job.php'); ?>
-<?php include_once('../templates/header.php'); ?>
+$search_results = [];
+
+if (isset($_POST["search-query"])) {
+    $search_query = $_POST["search-query"];
+
+    // Preparar la consulta SQL con una declaración preparada
+    $sql_search = "SELECT * FROM job_offers WHERE 
+                    title LIKE :query OR 
+                    company LIKE :query OR 
+                    description LIKE :query OR 
+                    location LIKE :query OR 
+                    requirements LIKE :query OR 
+                    salary LIKE :query";
+    $stmt = $pdo->prepare($sql_search);
+
+    // Comprobar si la preparación de la consulta fue exitosa
+    if ($stmt === false) {
+        die("Error en la preparación de la consulta: " . $pdo->errorInfo()[2]);
+    }
+
+    // Agregar comodines '%' a la consulta para buscar parcialmente
+    $search_query = '%' . $search_query . '%';
+
+    // Vincular los parámetros y ejecutar la consulta
+    $stmt->bindParam(':query', $search_query, PDO::PARAM_STR);
+    $stmt->execute();
+
+    // Obtener los resultados de la consulta
+    $search_results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// Resto de tu código
+
+include_once('../templates/php/info-job.php');
+include_once('../templates/header.php');
+?>
 
 <main>
 
     <div class="search-container-general">
         <div class="header-info">
             <a href="add-job-offers.php" class="post">Publicar Oferta</a>
-            <form action="/app/job-offers" method="POST" style="margin-top: 15px;">
+            <form action="" method="POST" style="margin-top: 15px;">
                 <input type="text" name="search-query" placeholder="Buscar..." class="search" />
                 <button type="submit" class="btn-search" title="Buscar ofertas"><i class="fa fa-search" id="searchIcon"></i></button>
                 <button type="submit" class="btn-filter" name="show-all-offers" title="Mostrar todas las ofertas"><i class="fas fa-filter"></i></button>
